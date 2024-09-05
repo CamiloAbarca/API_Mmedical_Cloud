c58f89b086d699a880db2b9c6c11eecb286dfc41
Camilo Abarca <camilo.abarca.21@gmail.com>
2024-09-04 22:53:48 -0400

    adds cors

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -4,6 +4,13 @@ import AuthController from '../controllers/AuthController';
 const authRoutes = Router()
 const controller = new AuthController()
 
+authRoutes.use((_req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Accept, Origin, X-Requested-With");
+    next();
+  });
+
 authRoutes.post('/login', controller.login)
 authRoutes.post('/register', controller.register)
 
