5df0443e5991b41ba99ec0918c6c65d3f09b11be
Camilo Abarca <camilo.abarca.21@gmail.com>
2024-09-06 19:37:02 -0400

    Correción de Cors

--- src/routes/equipoRoutes.ts
+++ src/routes/equipoRoutes.ts
@@ -7,7 +7,7 @@ const controller = new EquipoController()
 equipoRouters.use((_req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE");
-    res.header("Access-Control-Allow-Headers", "Content-Type, Accept, Origin, X-Requested-With");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Accept, Origin, X-Requested-With, Authorization");
     next();
   });
 
