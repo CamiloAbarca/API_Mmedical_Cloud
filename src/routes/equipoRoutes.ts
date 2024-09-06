15edf5340f4a95ecca53a1bc54c6e2fc38b5dab8
Camilo Abarca <camilo.abarca.21@gmail.com>
2024-09-06 17:43:56 -0400

    Add cors equipos

--- src/routes/equipoRoutes.ts
+++ src/routes/equipoRoutes.ts
@@ -4,6 +4,13 @@ import EquipoController from "../controllers/EquipoController";
 const equipoRouters = Router()
 const controller = new EquipoController()
 
+equipoRouters.use((_req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Accept, Origin, X-Requested-With");
+    next();
+  });
+
 equipoRouters.get('/', controller.getAll)
 equipoRouters.get('/:id', controller.getById)
 equipoRouters.post('/', controller.create)
