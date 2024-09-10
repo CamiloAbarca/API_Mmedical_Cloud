03ae716381af634050c171cf5fe5bcdf3b15c447
Camilo Abarca <camilo.abarca.21@gmail.com>
2024-09-09 23:01:17 -0300

    Correcions

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,8 @@
 import express from 'express'
 import morgan from 'morgan'
 import apiRoutes from './routes'
-import cors from 'cors'
 
 const app = express()
-
-app.use(cors({
-  origin: ['http://localhost:5173'], // allow requests from this origin
-  credentials: true, // allow credentials (e.g., cookies) to be sent
-}));
 
 app.use(express.json())
 app.use(morgan('dev'))
